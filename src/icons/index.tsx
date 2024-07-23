55a231108268cb7bb5315e8deb5f33eb537027c3
Ajonastech <Agujonas13@gmail.com>
2024-07-23 13:20:33 +0100

    style: preview documents

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -23,6 +23,9 @@ import Image from "next/image";
 export const HomeIcon = () => {
   return <Image src="/icons/home.svg" width={16} height={15.75} alt="T" />;
 };
+export const DownloadIcon =()=>{
+  return <Image src="/icons/download.svg" width={16} height={15.75} alt="T" />;
+}
 export const ChannelIcon = () => {
   return <Image src="/icons/channel.svg" width={16} height={15.75} alt="T" />;
 };
