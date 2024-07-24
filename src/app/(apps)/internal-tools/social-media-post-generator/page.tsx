0b58f403bc8c885da29ceb15e81250c28d8e579b
Ajonastech <Agujonas13@gmail.com>
2024-07-24 13:20:54 +0100

    style: weekly insghts

--- src/app/(apps)/internal-tools/social-media-post-generator/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div className='w-full h-full'><h4 className='heading-h4'>Coming Soon...</h4></div>
-  )
-}
-
-export default page
--- src/config/applicationConfig.ts
+++ src/config/applicationConfig.ts
@@ -1,89 +1,40 @@
 import EMPA1 from "../../public/assets/6801972f48246e41cf8f14062a7be104.png";
 import EMPA2 from "../../public/assets/d2bc26423822e9bfdb7643d99ad81a02.png";
-import EAI from "../../public/assets/aae5bcbec3e8d3af2aa39b5168351c51.png";
-import CB from "../../public/assets/c46d82c8e5ea86df0a7395ed132be53d.png";
-import RISKGPT from "../../public/assets/image-2.png";
-import RecommenderGPT from "../../public/assets/Recommender-GPT.png";
+import Translator from "../../public/assets/translator.svg"
+import weekly from "../../public/assets/weekly.svg";
+import RecommenderGPT from "../../public/assets/automated-issue.png";
 
 export const applications = [
-    {
-      img: EMPA1,
-      name: "Team Summarizer",
-      desc: "Effortlessly distill key insights from team meetings, saving time and ensuring everyone stays informed.",
-      text: "Hi there! Looking to gauge your project's potential for an EMPA? Whether it's assessing your sustainability goals, project size, or regulatory concerns, I'll guide you through.",
-      href:"/internal-tools/team-summarizer",
-      cards: [
-        {
-          heading: "Generate",
-          text: "EMPA Report on electricity distribution",
-        },
-        {
-          heading: "Create",
-          text: "EMPA for a Climatetech business",
-        },
-        {
-          heading: "Start",
-          text: "EMPA Analysis for waste cleaning programs",
-        },
-        {
-          heading: "Generate",
-          text: "EMPA for Sustainable Agriculture Projects",
-        },
-      ],
-    },
-    {
-      img: EMPA2,
-      name: "Newsletter",
-      desc: "Effortlessly curate and create engaging newsletters with our intuitive newsletter generator tool.",
-      text: "Hi there! Ready to optimize your EMPA journey? From data collection to analysis and report generation, I've got you covered. Let's streamline your environmental market positioning analysis together.",
-      href:"/internal-tools/newsletter",
-      cards: [
-        {
-          heading: "Show examples",
-          text: "of EMPA reports Climatetech businesses in Nigeria",
-        },
-        {
-          heading: "Write emails",
-          text: "to assist with client acquisition of EMPA projects",
-        },
-        {
-          heading: "Critically Analyse",
-          text: "EMPA reports on waste cleaning programs",
-        },
-        {
-          heading: "Generate",
-          text: "EMPA for Sustainable Agriculture Projects",
-        },
-      ],
-    },
-    {
-      img: EAI,
-      name: "Social Media Post Generator",
-      desc: "Generate captivating and engaging social media posts effortlessly, saving you time and boosting engagement.",
-      text: "Hi there! Need to document your environmental projects? Creates unique Environmental Asset Identifiers (EAIs) and detailed JSON structures, ensuring transparency and traceability for your carbon credit projects.",
-      href:"/internal-tools/social-media-post-generator",
-      cards: [
-        {
-          heading: "Generate",
-          text: "EAI for carbon emission projects",
-        },
-        {
-          heading: "Run an assessment",
-          text: "on the EAI for waste recycling projects",
-        },
-        {
-          heading: "Critically Analyse",
-          text: "EAI reports on waste cleaning programs",
-        },
-        {
-          heading: "Highlight",
-          text: "the competence of EAI on degeneration",
-        },
-      ],
-    },
+  
+   
+  {
+    img: weekly,
+    name: "Weekly Insights",
+    desc: "Effortlessly distill key insights from team meetings, saving time and ensuring everyone stays informed.",
+    text: "Hi there! Looking to streamline your project information gathering process? Whether it's data collection, analysis, or report generation.",
+    href:"/internal-tools/weekly-insights",
+    cards: [
+      {
+        heading: "Generate",
+        text: "project info for a Climatetech company",
+      },
+      {
+        heading: "Analyze",
+        text: "the project info attached for corrections",
+      },
+      {
+        heading: "Explore",
+        text: "the traditional style of writing project info",
+      },
+      {
+        heading: "Create",
+        text: "project info for a Climatetech company",
+      },
+    ],
+  },
   
     {
-      img: CB,
+      img: Translator,
       name: "Translator",
       desc: "Translate seamlessly and easily between languages, making communication across borders effortless.",
       text: "Hi there! Do you need to understand policies, compliance checks, or navigating regulatory frameworks for your business, I'm here to assist.  Share your queries, and I'll provide the guidance you need.",
@@ -104,31 +55,6 @@ export const applications = [
         {
           heading: "Generate Insights",
           text: "from the policies of a Climatetech business",
-        },
-      ],
-    },
-    {
-      img: RISKGPT,
-      name: "Project Info Generator",
-      desc: "Instantly generate comprehensive project details, simplifying your workflow and enhancing project management efficiency.",
-      text: "Hi there! Need to document your environmental projects? Creates unique Environmental Asset Identifiers (EAIs) and detailed JSON structures, ensuring transparency and traceability for your carbon credit projects.",
-      href:"/internal-tools/project-info-generator",
-      cards: [
-        {
-          heading: "Generate",
-          text: "EAI for carbon emission projects",
-        },
-        {
-          heading: "Run an assessment",
-          text: "on the EAI for waste recycling projects",
-        },
-        {
-          heading: "Critically Analyse",
-          text: "EAI reports on waste cleaning programs",
-        },
-        {
-          heading: "Highlight",
-          text: "the competence of EAI on degeneration",
         },
       ],
     },
