145ced5c9a128f64f6ee1c6f9f1083061f3dd883
Ajonastech <Agujonas13@gmail.com>
2024-08-05 11:31:07 +0100

    fix: ESlint on generatedEmpa page

--- src/components/GeneratedEmpa.tsx
+++ src/components/GeneratedEmpa.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-
 import Link from 'next/link';
 import Image from 'next/image';
 import GeneratedEmpaSkeleton from './skeletons/EmpaHistorySkeleton';
@@ -10,25 +9,43 @@ import { Report } from '@/types/empaReportsHistory';
 import { Button } from '@nextui-org/react';
 
 function EmpaOverview() {
-
-    const token = ".eJwNyt0OgiAYANB38QHcQPvxkpHTjwJbqYl3Sk5ByVYtJ09f5_p43cqGNlE60wwKB0hoeMPjslEUtjA-q5KyyP8npHC5VngawcxaGImFhVDk94G7PhQHMUkzoiwpFp4zy6103JVDfeP6RNnUpURnJg64gQ3P5SJcHfmENK82nNG5H5cYp-T4xQXNyWcOtG6VuTIbK1s3-52JvR9QTTeb.6j48mg0HadBMGhMWreuL2P0u33w"
+    const token = ".eJwNyt0OgiAYANB38QHcQPvxkpHTjwJbqYl3Sk5ByVYtJ09f5_p43cqGNlE60wwKB0hoeMPjslEUtjA-q5KyyP8npHC5VngawcxaGImFhVDk94G7PhQHMUkzoiwpFp4zy6103JVDfeP6RNnUpURnJg64gQ3P5SJcHfmENK82nNG5H5cYp-T4xQXNyWcOtG6VuTIbK1s3-52JvR9QTTeb.6j48mg0HadBMGhMWreuL2P0u33w";
 
     const { data: reports, isLoading, error } = useFetchData(["empa-reports"], () => getEmpaReports());
 
-
-
     if (error) {
-        console.log(error, "This is the error")
+        console.log(error, "This is the error");
     }
 
     return (
-        <main className="w-full min-h-full bg-white overflow-auto rounded-xl no-scrollbar flex flex-col gap-10  p-9 py-12   ">
-            {
-                (isLoading || reports.length !==0) && (
-                    <div className="flex justify-between items-center">
-                        <h6 className="heading-h6 font-semibold font-generalSans text-grey-700">
-                            EMPA Generator
-                        </h6>
+        <main className="w-full min-h-full bg-white overflow-auto rounded-xl no-scrollbar flex flex-col gap-10 p-9 py-12">
+            {(isLoading || reports.length !== 0) && (
+                <div className="flex justify-between items-center">
+                    <h6 className="heading-h6 font-semibold font-generalSans text-grey-700">
+                        EMPA Generator
+                    </h6>
+                    <Button
+                        as={Link}
+                        href="/EMPA/generate"
+                        aria-label="Generate EMPA"
+                        size="lg"
+                        className="font-medium text-grey-20 px-6 py-4 lg:text-lg bg-primary"
+                        color="primary"
+                    >
+                        Generate EMPA
+                    </Button>
+                </div>
+            )}
+            <div>
+                {/* render this if there is no data */}
+                {!isLoading && reports.length === 0 && (
+                    <div className='flex justify-center flex-col gap-3 w-full items-center'>
+                        <h2 className="!mb-0 heading-h2 font-semibold font-generalSans leading-[58.5px] text-G700">EMPA Generator</h2>
+                        <span className="w-[90%] text-center max-w-[600px] text-G100 text-lg leading-[25.2px] font-normal font-satoshi">
+                            Hello! How can I assist you today? Whether it&apos;s data collection,
+                            analysis, or report generation for your EMPA, I&apos;m here to help.
+                            Just let me know what you need.
+                        </span>
                         <Button
                             as={Link}
                             href="/EMPA/generate"
@@ -40,30 +57,7 @@ function EmpaOverview() {
                             Generate EMPA
                         </Button>
                     </div>
-                )
-            }
-            <div>
-                {/* render this if there is no data */}
-                {
-                    !isLoading && reports.length === 0 && (
-                        <div className='flex justify-center flex-col gap-3 w-full items-center'>
-                            <h2 className="  !mb-0 heading-h2 font-semibold font-generalSans leading-[58.5px] text-G700">EMPA Generator</h2>
-                            <span className=" w-[90%] text-center max-w-[600px] text-G100 text-lg leading-[25.2px] font-normal font-satoshi">
-                                Hello! How can I assist you today? Whether it's data collection, analysis, or report generation for your EMPA, I'm here to help. Just let me know what you need.
-                            </span>
-                            <Button
-                                as={Link}
-                                href="/EMPA/generate"
-                                aria-label="Generate EMPA"
-                                size="lg"
-                                className="font-medium text-grey-20 px-6 py-4 lg:text-lg bg-primary"
-                                color="primary"
-                            >
-                                Generate EMPA
-                            </Button>
-                        </div>
-                    )
-                }
+                )}
                 <div className="grid lg:grid-cols-2 xl:grid-cols-3 justify-start items-stretch !gap-4 !space-y-0 !sm:gap-2 sm:justify-center">
                     {isLoading ? (
                         // Display the loading skeleton
@@ -93,13 +87,11 @@ function EmpaOverview() {
                                     <span>Edited {report.lastEdited}</span>
                                 </div>
                             </Link>
-
                         ))
                     )}
                 </div>
             </div>
         </main>
-
     );
 }
 
