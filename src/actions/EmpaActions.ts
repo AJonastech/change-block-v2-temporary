9357a800619469281707a0cc4315e0cf2d54fd6f
Tachy-22 <entekumejeffrey@gmail.com>
2024-08-05 21:34:10 +0100

    history view

--- src/actions/EmpaActions.ts
+++ src/actions/EmpaActions.ts
@@ -1,13 +1,14 @@
-"use server"
+"use server";
 
-import { fetchData } from "@/lib/fetchData"
+import { fetchData } from "@/lib/fetchData";
 
-
-export async function getEmpaReports(){
- return await fetchData(`/v1/empa-reports`, 'GET')
-
+export async function getEmpaReports() {
+  return await fetchData(`/v1/empa-reports`, "GET");
+}
+export async function getEmpaReport(id: string) {
+  return await fetchData(`/v1/empa-reports/${id}`, "GET");
 }
 
-export async function createEmpaReport(data:any){
-    return fetchData(`/v1/empa-reports`, 'POST', data)
+export async function createEmpaReport(data: any) {
+  return fetchData(`/v1/empa-reports`, "POST", data);
 }
--- src/app/(apps)/EMPA/[segment]/page.tsx
+++ src/app/(apps)/EMPA/[segment]/page.tsx
@@ -1,23 +1,22 @@
 import EMPAReportHome from "@/components/EMPAReportHome";
 import EMPAReportSegment from "@/components/EMPAReportSegment";
 
-const page = ({
+const page = async ({
   params,
   searchParams,
 }: {
   params: { segment: string };
-  searchParams: { data: string; section: string };
+  searchParams: { data: string; id: string; section: string };
 }) => {
   const currentSegment = params?.segment;
+  const id = searchParams?.id;
 
   return (
     <div className=" w-full h-full  bg-white  rounded-2xl overflow-y-auto  no-scrollbar scroll-smooth outline-none">
       {currentSegment === "home" ? (
-        <EMPAReportHome />
+        <EMPAReportHome id={id} />
       ) : (
-        <EMPAReportSegment
-          section={searchParams.section}
-        />
+        <EMPAReportSegment section={searchParams.section} id={id}/>
       )}
     </div>
   );
