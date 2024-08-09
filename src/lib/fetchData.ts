cba06600c9ab4faed385a2ce5e6753ae1267e402
Ajonastech <Agujonas13@gmail.com>
2024-08-09 01:45:44 +0100

    should fix error message

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,6 +1,7 @@
+import { unstable_noStore as noStore } from "next/cache";
 import { getServerSession } from "./getServerSession";
 
-
+noStore()
 const baseUrl = process.env.BACKEND_URL;
 
 export async function fetchData(
